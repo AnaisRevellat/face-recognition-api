1f1b259630bf4dd2ee5f900d9fbbffc779c98b5a
Hexia <you@example.com>
2024-03-01 19:43:43 +0100

    making get and posts requests

--- server.js
+++ server.js
@@ -3,13 +3,71 @@ const express = require("express");
 const app = express();
 const port = 3500;
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "John",
+      email: "johnhowieson@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "124",
+      name: "Velka",
+      email: "velkawolf@gmail.com",
+      password: "matcha",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
 app.get("/", (req, res) => {
-  res.send("Hello World! Port is okay");
+  res.send(database.users);
 });
 
 app.post("/signin", (req, res) => {
-  res.json("signin");
+  if (
+    req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("error logging in");
+  }
 });
+
+app.post("/register", (req, res) => {
+  const { email, password, name } = req.body;
+  database.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+  res.json(database.users[database.users.length - 1]);
+});
+
+//get the profile for the homepage
+app.get("/profil/:id", (req, res) => {
+  const { id } = req.params;
+  database.users.forEach((user) => {
+    if (user.id === id) {
+      res.json(user);
+    } else {
+      res.status(404).json("no such user");
+    }
+  });
+});
+
+app.post("/image", (req, res) => {});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
