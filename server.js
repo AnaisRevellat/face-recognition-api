01288298e78cb4257084cb2cfd5689ed8cee9e70
Hexia <you@example.com>
2024-03-19 11:30:22 +0100

    finalizing signin and register

--- server.js
+++ server.js
@@ -65,14 +65,22 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json("error logging in");
-  }
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        db.select("*")
+          .from("users")
+          .where("email", "=", req.body.email)
+          .then((user) => {
+            res.json(user[0]);
+          })
+          .catch((err) => res.status(400).json("unable to get user"));
+      }
+    })
+    .catch((err) => res.status(400).json('wrong credentials'));
 });
 
 app.post("/register", (req, res) => {
@@ -80,18 +88,28 @@ app.post("/register", (req, res) => {
   const hash = bcrypt.hashSync(password);
   db.transaction((trx) => {
     //here a transaction is important to ensure that the process is okay and in case of failure we go back
-  });
-  db("users")
-    .returning("*") //in the knex docs returning feature allows us to easily send the response
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date(),
-    })
-    .then((user) => {
-      res.json(user[0]);
-    })
-    .catch((err) => res.status(400).json("unable to register"));
+    trx
+      .insert({
+        hash: hash,
+        email: email,
+      })
+      .into("login")
+      .returning("email")
+      .then((loginEmail) => {
+        return trx("users")
+          .returning("*") //in the knex docs returning feature allows us to easily send the response
+          .insert({
+            email: loginEmail[0].email,
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json("unable to register"));
 });
 
 //get the profile for the homepage
